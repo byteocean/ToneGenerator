c4486c07b1bec87924b070eb1f85e974fa767320
Aaron <ntlitao@gmail.com>
2012-02-04 09:54:36 +0100

    Codes works at lab time. Add control function to increase/decrease the loopRange of background task.TODO: pack loopRange into class.

--- partone.c
+++ partone.c
@@ -1,36 +1,42 @@
 #include "TinyTimber.h"
 #include "parallelportTinyTimber.h"
+#include "sciTinyTimber.h"
+#include "canTinyTimber.h"
 
 #include <machine/hcs12.h>
 
 #define DATA_OUT 0xFF //write to PortP's DDRP to set PortP as output
-#define BACKGROUND_LOOP_RANGE 500
+//#define BACKGROUND_LOOP_RANGE 150
+int loopRange = 0;
 
 typedef struct{
 	Object super;
 	unsigned char direction;
 	unsigned char flip;
 	Time period;
+	Time deadline;	
 } ToneGenerator;
 
-#define initToneGenerator() {initObject(),DATA_OUT,0x00,0}
+#define initToneGenerator() {initObject(),DATA_OUT,0x00,0,0}
 
 ToneGenerator toneGenerate = initToneGenerator();
 
-ParallelPort portDevice = initParallelPort(PORT,&toneGenerate);
+ParallelPort portDevice = initParallelPort(PORT);
 
 
 void generateTone(ToneGenerator *,int);
 void initDevice(ToneGenerator *self,int unused){
 	self -> period = USEC(500);
-	PARALLEL_PORT_INIT(&portDevice,(int)self -> direction);
-	generateTone(self,0);
+	self -> deadline = USEC(100);
+	PARALLEL_PORT_INIT(&portDevice,(int)(self -> direction));
+	ASYNC(self,generateTone,0);
 }
 
 void generateTone(ToneGenerator *self,int unused){
-	~(self -> flip);
-	PARALLEL_PORT_WRITE(&portDevice,(int)self -> flip);
-	SEND(self -> period,0,self,generateTone,0);
+	(self -> flip)=(self -> flip)^0x01;;
+	PARALLEL_PORT_WRITE(&portDevice,(int)(self -> flip));
+	SEND(self -> period,self -> deadline,self,generateTone,0);
+	//AFTER(self -> period,self,generateTone,0);
 }
 
 //TODO: pack the generator as one class
@@ -38,33 +44,37 @@ void generateTone(ToneGenerator *self,int unused){
 //create background task
 typedef struct{
 	Object obj;
-	float number;
-	float result;
+	int number;
+	int result;
+      //int loopRange;
 	Time period;
+	Time deadline;
 } Multiplier;
 
-#define initMultiplier() {initObject(),1.5,1.0,0}
+#define initMultiplier() {initObject(),2,1,0,0}
 
 Multiplier backGround = initMultiplier();
 
 void multiply(Multiplier *,int);
 void setPeriod(Multiplier *self, int unused){
 	self -> period = USEC(1300);
-	multiply(self,0);
+	self -> deadline = USEC(1300);
+	ASYNC(self,multiply,0);
 }
 void multiply(Multiplier *self,int unused){
 	int count=0;
-	float tmp;
-	self -> result = 1.0;	
+	int tmp=2;
+	//self -> result = 1.0;	
 	
-	do{
-	tmp = self -> result;
-	tmp += self -> number;
-	self -> result = tmp;
+	while(count<loopRange){
+	//tmp = self -> result;
+	//tmp += self -> number;
+	//(self -> result) = tmp;
 	count++;
-	}while(count<BACKGROUND_LOOP_RANGE);
+	}
 	
-	SEND(self -> period,0,self,multiply,0);
+	SEND(self -> period,self -> deadline,self,multiply,0);
+	//AFTER(self -> period,self,multiply,0);
 }	
 	
 
@@ -75,13 +85,44 @@ typedef struct{
 
 StartApp app = {initObject()};
 
+void reader(StartApp*, int);
+void receiver(StartApp*, int);
+
+Can can0 = initCan(CAN0BASE, &app, receiver);
+Serial sci0 = initSerial(SCI_PORT0, &app, reader);
+
+void receiver(StartApp *self, int unused) {
+    CANMsg msg;
+    CAN_RECEIVE(&can0, &msg);
+    SCI_WRITE(&sci0, "Can msg received: ");
+    SCI_WRITE(&sci0, msg.buff);
+ 
+}
+
+void reader(StartApp *self, int c) {
+    SCI_WRITE(&sci0, "Rcv: \'");
+    SCI_WRITECHAR(&sci0, c);
+    SCI_WRITE(&sci0, "\'\n");
+    if(c=='i'){
+	loopRange += 50;
+    }else if(c=='d'){
+	if (loopRange > 0){
+		loopRange -=50;
+	}
+    }
+}
+
 void kickoff (StartApp *obj,int unused){
+	SCI_INIT(&sci0);
+	CAN_INIT(&can0);
 	ASYNC(&toneGenerate,initDevice,0);
 	ASYNC(&backGround,setPeriod,0);
 }
 
 
 int main(){
+	INSTALL(&sci0, sci_interrupt, SCI_IRQ0);
+	INSTALL(&can0, can_interrupt, CAN_IRQ0);
 	TINYTIMBER(&app,kickoff,0);
 	return 0;
 }
